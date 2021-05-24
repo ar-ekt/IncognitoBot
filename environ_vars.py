e4d6020b87b54e716128115750651d60ca0bbe81
Amirreza Ektesabi <ar.ektesabi@gmail.com>
2021-05-24 14:37:28 +0430

    Update environ_vars.py

--- environ_vars.py
+++ environ_vars.py
@@ -11,5 +11,5 @@ CHANNEL_NAME = "CseIncognito" # os.environ.get("CHANNEL_NAME")
 PRIVATE_CHANNEL_ID = int(os.environ.get("PRIVATE_CHANNEL_ID"))
 
 WEB_URL = "incognito.c1.biz" # os.environ.get("WEB_URL")
-WEB_API_TOKEN = int(os.environ.get("WEB_API_TOKEN"))
+WEB_API_TOKEN = os.environ.get("WEB_API_TOKEN")
 WEB_REQUEST_TIME = int(os.environ.get("WEB_REQUEST_TIME")) # minute
